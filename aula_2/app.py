804a70415e6f044ab27259182c55dd0e02e8ef01
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-08-07 21:14:09 -0300

    2113 07082019

--- aula_2/app.py
+++ aula_2/app.py
@@ -1,3 +1,22 @@
 
+import sys
+import os
 
-import routes
+import flask
+
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+	context = {
+		'title': 'Python | SysAdmin'
+	}
+	return flask.render_template('index.html', context = context)
+
+if __name__ == "__main__":
+
+	root_module = os.path.abspath(os.path.curdir)
+	sys.path.append(root_module)
+
+	app.run(host='0.0.0.0')
