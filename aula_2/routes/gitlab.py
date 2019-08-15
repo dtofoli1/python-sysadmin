d3d2b894a94f9d1bb4bae39afa76bec70a4f72c2
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-08-14 22:23:16 -0300

    Aula 08

--- aula_2/routes/gitlab.py
+++ aula_2/routes/gitlab.py
@@ -1,11 +1,32 @@
+#DSywR2EWMKACytNPED5T
 
 import flask
+import requests
 
 blueprint = flask.Blueprint('gitlab', __name__)
 
 @blueprint.route('/gitlab', methods=['GET', 'POST'])
 def gitlab():
+	
+	users = requests.get('http://localhost:8000/api/v4/users', headers = { 'Private-Token': 'tvivjqsWcJbsezuRzozU' })
+	projects = requests.get('http://localhost:8000/api/v4/projects', headers = { 'Private-Token': 'tvivjqsWcJbsezuRzozU' })
+
 	context = {
-		'title': 'Python | SysAdmin'
+		'title': 'Python | SysAdmin',
+		'users': users.json() if users.status_code == 200 else [],
+		'projects': projects.json() if projects.status_code == 200 else []
 	}
 	return flask.render_template('gitlab.html', context=context)
+
+@blueprint.route('/gitlab/<projectid>', methods=[ 'GET', 'POST'])
+def gitlab_commit(projectid):
+
+	URL = 'http://localhost:8000/api/v4/projects/{}/repository/commits/'.format(projectid)
+	commits = requests.get(URL, headers={'Private-Token': 'tvivjqsWcJbsezuRzozU' })
+
+	context = {
+		'title': 'Python | Sysadmin',
+		'commits': commits.json() if commits.status_code == 200 else []
+	}
+
+	return flask.render_template('gitlab_commits.html', context=context)
